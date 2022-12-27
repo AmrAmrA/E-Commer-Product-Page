edc71d5058d8ca564af34b20dde1be5bf2f6c698
Abd El Fattah Amr <amrelfattah.dev@gmail.com>
2022-12-27 16:39:28 +0100

    Add the filter for the mobile Menu

--- app.js
+++ app.js
@@ -1,16 +1,23 @@
 let menuIcon = document.querySelector(".hamburger__menu");
 let mobileMenu = document.querySelector(".mobile__menu");
 let crossSign = document.querySelector(".cross__sign");
-console.log(crossSign);
+let filter = document.querySelector(".globalFilter");
+console.log(filter);
 
 menuIcon.addEventListener("click", menuAppearance);
 crossSign.addEventListener("click", menuDisppearance);
+mobileMenu.classList.remove("appearance");
 
 function menuAppearance() {
   mobileMenu.classList.add("appearance");
   mobileMenu.classList.remove("disAppearance");
+
+  filter.classList.add("toggleVisibilty");
 }
 function menuDisppearance() {
   mobileMenu.classList.add("disAppearance");
   mobileMenu.classList.remove("appearance");
+  filter.classList.remove("toggleVisibilty");
 }
+
+console.log(document.body);
